74a976c6483501bc99d3a0a0ed30ef1cf655235c
a6b8 <andreasbanholzer@me.com>
2021-06-18 07:21:38 +0200

    v0.04

--- titles.rb
+++ titles.rb
@@ -1,8 +1,8 @@
 require 'markdown_titles_to_svg'
 
 markdowns = [
-    'https://raw.githubusercontent.com/a6b8/statosio-for-ruby/blob/main/README.md'
-    #'https://raw.githubusercontent.com/a6b8/a6b8/main/README.md',
+    'https://raw.githubusercontent.com/a6b8/statosio-for-ruby/main/README.md',
+    'https://raw.githubusercontent.com/a6b8/a6b8/main/README.md',
     #'https://raw.githubusercontent.com/a6b8/ascii-to-svg-generator-for-ruby/main/README.md'
 ]
 
