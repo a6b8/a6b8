25d50b53bba93259997ab3116ab18a4a984ac82a
a6b8 <andreasbanholzer@me.com>
2021-06-19 00:15:38 +0200

    update banner

--- titles.rb
+++ titles.rb
@@ -10,7 +10,7 @@ MarkdownTitlesToSvg.generate(
     markdowns,
     'a6b8',
     {
-        style__color__default: '#EA8872'
+        #style__color__default: '#EA8872'
         #font__source__google_fonts__name: 'Alfa Slab One'
     }
 )
