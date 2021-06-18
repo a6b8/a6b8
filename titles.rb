14905bc37739a4222d30cf77269706e6e1985cdb
a6b8 <andreasbanholzer@me.com>
2021-06-19 00:17:55 +0200

    update banner

--- titles.rb
+++ titles.rb
@@ -3,7 +3,7 @@ require 'markdown_titles_to_svg'
 markdowns = [
     'https://raw.githubusercontent.com/a6b8/statosio-for-ruby/main/README.md',
     'https://raw.githubusercontent.com/a6b8/a6b8/main/README.md',
-    #'https://raw.githubusercontent.com/a6b8/ascii-to-svg-generator-for-ruby/main/README.md'
+    'https://raw.githubusercontent.com/a6b8/ascii-to-svg-generator-for-ruby/main/README.md'
 ]
 
 MarkdownTitlesToSvg.generate( 
