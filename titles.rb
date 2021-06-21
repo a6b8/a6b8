76e102186632fff1dbdc3da6c541950c99a46730
a6b8 <andreasbanholzer@me.com>
2021-06-21 22:17:21 +0200

    update banner

--- titles.rb
+++ titles.rb
@@ -7,10 +7,10 @@ markdowns = [
     'https://raw.githubusercontent.com/a6b8/statosio-for-wordpress/main/README.md',
     'https://raw.githubusercontent.com/a6b8/statosio.js/main/README.md',
     'https://raw.githubusercontent.com/a6b8/statosio-sandbox/main/README.md',
-    'https://raw.githubusercontent.com/a6b8/a6b8/main/templates/readme.md',
+    #'https://raw.githubusercontent.com/a6b8/a6b8/main/templates/readme.md',
     'https://raw.githubusercontent.com/a6b8/markdown-titles-to-svg-for-ruby/main/README.md',
     'https://raw.githubusercontent.com/a6b8/local-path-builder-for-ruby/main/README.md',
-    #'https://raw.githubusercontent.com/a6b8/documents-with-footer-to-pdf/main/README.md'
+    'https://raw.githubusercontent.com/a6b8/documents-with-footer-to-pdf/main/README.md'
 ]
 
 MarkdownTitlesToSvg.generate( 
