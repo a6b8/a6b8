be054d0c708f3b99a5865128e9a99a68cde721a8
a6b8 <andreasbanholzer@me.com>
2022-01-11 21:53:12 +0100

    update banner

--- profile-upload.sh
+++ profile-upload.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+
+echo "Create Headers"
 ruby ./__generate/profile_headlines.rb
+
+echo "Generate Profile"
 ruby ./__generate/profile.rb
+
+echo "Upload to Github"
 git add .
 git commit -m 'update banner'
 git push -u origin main --force
