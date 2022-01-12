ad60b8b9e4286ddbaa443a2edabe4e183e98630f
a6b8 <andreasbanholzer@me.com>
2022-01-12 11:38:44 +0100

    update banner

--- __generate/modules/repositories.rb
+++ __generate/modules/repositories.rb
@@ -125,10 +125,10 @@ module Repositories
                             .gsub( '{{gem_name}}', item[:gem] )
                     end
                 when :javascript
-                    if item.keys.include? 'js'
-                        result = obj[:endpoints][:cdnjs]
-                            .gsub( '{{gem_name}}', item[:js] )
-                    end
+                 #   if item.keys.include? 'js'
+                 #       result = obj[:endpoints][:cdnjs]
+                 #           .gsub( '{{gem_name}}', item[:js] )
+                 #   end
                 when :php
             else
                 puts "#{item[:type]} not set."
