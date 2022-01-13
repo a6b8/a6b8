039b6daaeaa5ffaecddb17289d97de3e2b85af6f
a6b8 <andreasbanholzer@me.com>
2022-01-13 23:26:08 +0100

    update banner

--- __generate/modules/demonstrations.rb
+++ __generate/modules/demonstrations.rb
@@ -2,10 +2,71 @@ module Demonstrations
     def self.generate( struct )
         file = File.read( struct )
         hash = JSON.parse( file ).with_indifferent_access
+        prepares = prepares( data: hash )
+        markdown = to_markdown( prepares: prepares )
 
-        puts hash
-
-        return ''
+        return markdown
     end
 
+
+    def self.prepares( data: )
+        items = []
+
+        data[:projects].each do | project |
+            item = {
+                name: nil
+                tags: nil,
+                year: nil
+                url: nil
+            }
+
+            name = project[:repo]
+                .split( '-' )
+                .map { | a | "#{a[ 0, 1 ].upcase}#{a[ 1, a.length ]}" }
+                .join( ' ' )
+
+            item[:name] = "[#{name}](https://github.com/#{data[:meta][:github_user]})"
+            
+            item[:url] = "https://#{data[:meta][:github_user]}.github.io/#{project[:repo]}/"
+
+            item[:tags] = project[:tags]
+                .map { | a | "[#{a}](https://github.com/search?q=#{a})" }
+                .join( ', ' )
+
+            items.push( item )
+        end
+
+        return items
+    end
+
+
+    def self.to_markdown( prepares: )
+        strs = []
+        str = prepares[ 0 ]
+            .keys
+            .map { | a | "#{a.to_s[0, 1].upcase}#{a.to_s[ 1, a.length ]}" }
+            .join( ' |' )
+            .concat( ' |' )
+            .insert( 0, '| ')
+        strs.push( str )
+         
+        str = prepares[ 0 ].keys.length.times.to_a
+            .map { | a | ":---" }
+            .join( ' |' )
+            .concat( ' |' )
+            .insert( 0, '| ')
+        strs.push( str )
+
+        prepares.each do | prepare |
+            str = prepare
+                .map { | k, v | v }
+                .join( ' |' )
+                .concat( ' |' )
+                .insert( 0, '| ')
+            strs.push( str )
+        end
+
+
+        return strs.join( "\n" )
+    end
 end
