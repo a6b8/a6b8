ad78dfc3ba841bd1559f4717a722d65fd3746cb4
αηdr3α5 ɮαηɦօlʐ3ʀ <AndreasBanholzer@Me.com>
2025-01-25 16:33:25 +0100

    Setup data section

--- src/data/sections.mjs
+++ src/data/sections.mjs
@@ -11,7 +11,7 @@ export const sections = {
                     'url': ''
                 }
             ]
-        }
+        },
         'solana': {
             'title': 'Solana',
             'preset': 'githubOverviewWithArticle',
